9b0172b08825f50f45a889be931ddd7e35c8c227
Lexandrina90 <lexandrina90@gmail.com>
2022-08-11 13:13:25 +0300

    setState

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from 'react-dom/client';
 class App extends React.Component {
     constructor(props) {
         super(props);
-
+        //ЭТО ЕДИНСТВЕННЫЙ РАЗ, КОГДА МЫ НАПРЯМУЮ ЗАДАЁМ this.state
         this.state = { lat: null };
-    }
-    render() {
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                //мы вызвали setState!!!
+               this.setState({lat: position.coords.latitude });
+               //так неправильно !!! - this.state.lat = position.coords.latitude
+
+            },
             (err) => console.log(err)
         );
-        return <div>Latitude: </div>
+    }
+    render() {
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
