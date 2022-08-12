7f45f940903a8d554bba431f7980502c4a5dcae5
Lexandrina90 <lexandrina90@gmail.com>
2022-08-12 18:27:02 +0300

    componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        //ЭТО ЕДИНСТВЕННЫЙ РАЗ, КОГДА МЫ НАПРЯМУЮ ЗАДАЁМ this.state
-        this.state = { lat: null, errorMessage: '' };
+    state = {lat: null, errorMessage: ''};
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //мы вызвали setState!!!
-               this.setState({ lat: position.coords.latitude });
-               //так неправильно !!! - this.state.lat = position.coords.latitude
-
-            },
-            (err) => {
-               this.setState({ errorMessage: err.message }); 
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
         return <div>Loading!</div>
     }
